fbbe877922e22aa375df69bef0c59f35259bb6a6
Loc Nguyen <locnguyen230598@gmail.com>
2019-04-08 13:49:15 +0700

    update display

--- Model/EF/Content.cs
+++ Model/EF/Content.cs
@@ -1,4 +1,4 @@
-namespace Model.EF
+﻿namespace Model.EF
 {
     using System;
     using System.Collections.Generic;
@@ -10,51 +10,51 @@ namespace Model.EF
     public partial class Content
     {
         public long ID { get; set; }
-
+        [Display(Name ="Tên Sản Phẩm")]
         [StringLength(250)]
         public string Name { get; set; }
-
+        [Display(Name ="Đường Dẫn")]
         [StringLength(250)]
         public string MetaTitle { get; set; }
-
+        [Display(Name ="Miêu Tả")]
         [StringLength(500)]
         public string Description { get; set; }
-
+        [Display(Name ="Hình Ảnh")]
         [StringLength(250)]
         public string Image { get; set; }
-
+        [Display(Name ="Loại")]
         public long? CategoryID { get; set; }
-
+        [Display(Name ="Chi Tiết")]
         [Column(TypeName = "ntext")]
         public string Detail { get; set; }
-
+        [Display(Name ="")]
         public int? Warranty { get; set; }
-
+        [Display(Name ="Ngày Tạo")]
         public DateTime? CreatedDate { get; set; }
-
+        [Display(Name ="Người Tạo")]
         [StringLength(50)]
         public string CreatedBy { get; set; }
-
+        [Display(Name ="Ngày Sửa Đổi")]
         public DateTime? ModifiedDate { get; set; }
-
+        [Display(Name ="Người Sửa Đổi")]
         [StringLength(50)]
         public string ModifiedBy { get; set; }
-
+        [Display(Name ="")]
         [StringLength(250)]
         public string MetaKeywords { get; set; }
-
+        [Display(Name ="Mô Tả")]
         [StringLength(250)]
         public string MetaDescriptions { get; set; }
-
+        [Display(Name ="Trạng Thái")]
         public bool Status { get; set; }
-
+        [Display(Name ="Nổi Bật")]
         public DateTime? TopHot { get; set; }
-
+        [Display(Name ="Lượt Xem")]
         public int? ViewCount { get; set; }
-
+        [Display(Name ="Thẻ")]
         [StringLength(500)]
         public string Tags { get; set; }
-
+        [Display(Name ="Ngôn Ngữ")]
         public string Language { set; get; }
     }
 }
--- Model/EF/Product.cs
+++ Model/EF/Product.cs
@@ -1,4 +1,4 @@
-namespace Model.EF
+﻿namespace Model.EF
 {
     using System;
     using System.Collections.Generic;
@@ -10,60 +10,60 @@ namespace Model.EF
     public partial class Product
     {
         public long ID { get; set; }
-
+        [Display(Name="Tên Sản Phẩm")]
         [StringLength(250)]
         public string Name { get; set; }
-
+        [Display(Name="Mã Sản Phẩm")]
         [StringLength(10)]
         public string Code { get; set; }
-
+        [Display(Name="Đường Dẫn")]
         [StringLength(250)]
         public string MetaTitle { get; set; }
-
+        [Display(Name="Miêu Tả")]
         [StringLength(500)]
         public string Description { get; set; }
-
+        [Display(Name ="Hình Ảnh")]
         [StringLength(250)]
         public string Image { get; set; }
-
+        [Display (Name ="Thêm Hình Ảnh")]
         [Column(TypeName = "xml")]
         public string MoreImages { get; set; }
-
+        [Display(Name ="Gía Bán")]
         public decimal? Price { get; set; }
-
+        [Display(Name ="Gía Khuyến Mãi")]
         public decimal? PromotionPrice { get; set; }
-
+        [Display(Name ="Thuế VAT")]
         public bool? IncludedVAT { get; set; }
-
+        [Display(Name ="Số Lượng")]
         public int Quantity { get; set; }
-
+        [Display(Name ="Loại")]
         public long? CategoryID { get; set; }
-
+        [Display(Name ="Chi Tiết")]
         [Column(TypeName = "ntext")]
         public string Detail { get; set; }
-
+        [Display(Name="")]
         public int? Warranty { get; set; }
-
+        [Display(Name ="Ngày Tạo")]
         public DateTime? CreatedDate { get; set; }
-
+        [Display(Name ="Người Tạo")]
         [StringLength(50)]
         public string CreatedBy { get; set; }
-
+        [Display(Name ="Ngày Sửa Đổi")]
         public DateTime? ModifiedDate { get; set; }
-
+        [Display(Name ="Người Sửa Đổi")]
         [StringLength(50)]
         public string ModifiedBy { get; set; }
-
+        [Display(Name ="")]
         [StringLength(250)]
         public string MetaKeywords { get; set; }
-
+        [Display(Name ="")]
         [StringLength(250)]
         public string MetaDescriptions { get; set; }
-
+        [Display(Name ="Trạng Thái")]
         public bool? Status { get; set; }
-
+        [Display(Name ="Nổi Bật")]
         public DateTime? TopHot { get; set; }
-
+        [Display(Name ="Lượt Xem")]
         public int? ViewCount { get; set; }
     }
 }
