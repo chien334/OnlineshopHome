5a0d48daa49cbb2265bb8eeed0dda9c8e9ca0cfb
lê quyết chiên <lequyetchien3498@gmail.com>
2019-04-08 22:44:05 +0700

    update

--- Model/EF/Product.cs
+++ Model/EF/Product.cs
@@ -10,60 +10,6 @@
     public partial class Product
     {
         public long ID { get; set; }
-<<<<<<< HEAD
-        [Display(Name = "Tên Sản Phẩm")]
-        [StringLength(250)]
-        public string Name { get; set; }
-        [Display(Name = "Mã Sản Phẩm")]
-        [StringLength(10)]
-        public string Code { get; set; }
-        [Display(Name = "Đường Dẫn")]
-        [StringLength(250)]
-        public string MetaTitle { get; set; }
-        [Display(Name = "Miêu Tả")]
-        [StringLength(500)]
-        public string Description { get; set; }
-        [Display(Name = "Hình Ảnh")]
-        [StringLength(250)]
-        public string Image { get; set; }
-        [Display(Name = "Thêm Hình Ảnh")]
-        [Column(TypeName = "xml")]
-        public string MoreImages { get; set; }
-        [Display(Name = "Gía Bán")]
-        public decimal? Price { get; set; }
-        [Display(Name = "Gía Khuyến Mãi")]
-        public decimal? PromotionPrice { get; set; }
-        [Display(Name = "Thuế VAT")]
-        public bool? IncludedVAT { get; set; }
-        [Display(Name = "Số Lượng")]
-        public int Quantity { get; set; }
-        [Display(Name = "Loại")]
-        public long? CategoryID { get; set; }
-        [Display(Name = "Chi Tiết")]
-        [Column(TypeName = "ntext")]
-        public string Detail { get; set; }
-        [Display(Name = "Thời hạn bảo hành")]
-        public int? Warranty { get; set; }
-        [Display(Name = "Ngày Tạo")]
-        public DateTime? CreatedDate { get; set; }
-        [Display(Name = "Người Tạo")]
-        [StringLength(50)]
-        public string CreatedBy { get; set; }
-        [Display(Name = "Ngày Sửa Đổi")]
-        public DateTime? ModifiedDate { get; set; }
-        [Display(Name = "Người Sửa Đổi")]
-        [StringLength(50)]
-        public string ModifiedBy { get; set; }
-        [StringLength(250)]
-        public string MetaKeywords { get; set; }
-        [StringLength(250)]
-        public string MetaDescriptions { get; set; }
-        [Display(Name = "Trạng Thái")]
-        public bool? Status { get; set; }
-        [Display(Name = "Nổi Bật")]
-        public DateTime? TopHot { get; set; }
-        [Display(Name = "Lượt Xem")]
-=======
         [Display(Name="Tên Sản Phẩm")]
         [StringLength(250)]
         public string Name { get; set; }
@@ -118,7 +64,6 @@
         [Display(Name ="Nổi Bật")]
         public DateTime? TopHot { get; set; }
         [Display(Name ="Lượt Xem")]
->>>>>>> 7ea5797da8734fc4dbb37db77ac155de09f122ce
         public int? ViewCount { get; set; }
     }
 }
--- Model/EF/User.cs
+++ Model/EF/User.cs
@@ -8,7 +8,6 @@
     public partial class User
     {
         public long ID { get; set; }
-<<<<<<< HEAD
         [Display(Name = "Tên Người Dùng")]
         [StringLength(50)]
         public string UserName { get; set; }
@@ -45,44 +44,6 @@
         [StringLength(50)]
         public string ModifiedBy { get; set; }
         [Display(Name = "Trạng Thái")]
-=======
-        [Display(Name ="Tên Người Dùng")]
-        [StringLength(50)]
-        public string UserName { get; set; }
-        [Display(Name ="Mật Khẩu")]
-        [StringLength(32)]
-        public string Password { get; set; }
-        [Display(Name ="Nhóm Người Dùng")]
-        [StringLength(20)]
-        public string GroupID { set; get; }
-        [Display(Name ="Tên")]
-        [StringLength(50)]
-        public string Name { get; set; }
-        [Display(Name ="Địa Chỉ Nhà")]
-        [StringLength(50)]
-        public string Address { get; set; }
-        [Display(Name ="E-Mail")]
-        [StringLength(50)]
-        public string Email { get; set; }
-        [Display(Name ="Điện Thoại")]
-        [StringLength(50)]
-        public string Phone { get; set; }
-        [Display(Name ="Tỉnh Thành ")]
-        public int? ProvinceID { set; get; }
-        [Display(Name ="Huyện")]
-        public int? DistrictID { set; get; }
-        [Display(Name ="Ngày Tạo")]
-        public DateTime? CreatedDate { get; set; }
-        [Display(Name ="Người Tạo")]
-        [StringLength(50)]
-        public string CreatedBy { get; set; }
-        [Display(Name ="Ngày Sửa Đổi")]
-        public DateTime? ModifiedDate { get; set; }
-        [Display(Name ="Người Sửa Đổi")]
-        [StringLength(50)]
-        public string ModifiedBy { get; set; }
-        [Display(Name ="Trạng Thái")]
->>>>>>> 7ea5797da8734fc4dbb37db77ac155de09f122ce
         public bool Status { get; set; }
     }
 }
